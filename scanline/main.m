b55f85417ed460105aadc0e5acf13528a48b51ea
Scott J. Kleper <klep@contextoptional.com>
2012-09-08 14:22:07 -0700

    Initial Commit

---
+++ scanline/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  scanline
+//
+//  Created by Scott J. Kleper on 9/8/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
