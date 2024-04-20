511018d6d90b28e19ffa06e7111e257441c00a40
Scott J. Kleper <s@klep.me>
2024-04-20 12:06:45 -0400

    cleanup

--- libscanline/ScanlineOutputProcessor.swift
+++ libscanline/ScanlineOutputProcessor.swift
@@ -230,33 +230,3 @@ fileprivate extension Int {
         return String(format: "%ld", self)
     }
 }
-
-fileprivate extension CIImage {
-    
-}
-//fileprivate extension UIImage {
-//    // HT: https://stackoverflow.com/questions/27092354/rotating-uiimage-in-swift
-//    func rotate(degrees: Int) -> UIImage? {
-//        let radians = CGFloat(degrees) / 180.0 * CGFloat(M_PI)
-//
-//        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
-//        // Trim off the extremely small float value to prevent core graphics from rounding it up
-//        newSize.width = floor(newSize.width)
-//        newSize.height = floor(newSize.height)
-//
-//        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
-//        guard let context = UIGraphicsGetCurrentContext() else { return nil }
-//
-//        // Move origin to middle
-//        context.translateBy(x: newSize.width/2, y: newSize.height/2)
-//        // Rotate around middle
-//        context.rotate(by: CGFloat(radians))
-//        // Draw the image at its center
-//        self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
-//
-//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//
-//        return newImage
-//    }
-//}
